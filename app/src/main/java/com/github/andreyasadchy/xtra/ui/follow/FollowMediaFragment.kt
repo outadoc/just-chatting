3e458d94c0c380e01840b9ae0a445531c18a9651
crackededed <90209774+crackededed@users.noreply.github.com>
2022-03-31 18:01:34 +0300

    dock crash fix

--- app/src/main/java/com/github/andreyasadchy/xtra/ui/follow/FollowMediaFragment.kt
+++ app/src/main/java/com/github/andreyasadchy/xtra/ui/follow/FollowMediaFragment.kt
@@ -1,5 +1,6 @@
 package com.github.andreyasadchy.xtra.ui.follow
 
+import android.os.Bundle
 import androidx.fragment.app.Fragment
 import com.github.andreyasadchy.xtra.R
 import com.github.andreyasadchy.xtra.ui.common.MediaFragment
@@ -9,22 +10,36 @@ import com.github.andreyasadchy.xtra.ui.streams.followed.FollowedStreamsFragment
 import com.github.andreyasadchy.xtra.ui.videos.followed.FollowedVideosFragment
 import kotlinx.android.synthetic.main.fragment_media.*
 
-class FollowMediaFragment(
-    private val followPager: Boolean,
-    private val defaultItem: Int?,
-    private val loggedIn: Boolean) : MediaFragment() {
+class FollowMediaFragment : MediaFragment() {
+
+    companion object {
+        private const val FOLLOW_PAGER = "follow_pager"
+        private const val DEFAULT_ITEM = "default_item"
+        private const val LOGGED_IN = "logged_in"
+
+        fun newInstance(followPager: Boolean, defaultItem: Int?, loggedIn: Boolean) = FollowMediaFragment().apply {
+            arguments = Bundle().apply {
+                putBoolean(FOLLOW_PAGER, followPager)
+                putInt(DEFAULT_ITEM, defaultItem ?: 0)
+                putBoolean(LOGGED_IN, loggedIn)
+            }
+        }
+    }
 
     private var firstLaunch = true
 
     override val spinnerItems: Array<String>
-        get() = resources.getStringArray(if (loggedIn) R.array.spinnerFollowedEntries else R.array.spinnerFollowedEntriesNotLoggedIn)
+        get() = resources.getStringArray(if (requireArguments().getBoolean(LOGGED_IN)) R.array.spinnerFollowedEntries else R.array.spinnerFollowedEntriesNotLoggedIn)
 
     override fun onSpinnerItemSelected(position: Int): Fragment {
+        val followPager = requireArguments().getBoolean(FOLLOW_PAGER)
+        val defaultItem = requireArguments().getInt(DEFAULT_ITEM)
+        val loggedIn = requireArguments().getBoolean(LOGGED_IN)
         if (followPager) {
             hideSpinner = true
-            return FollowPagerFragment(defaultItem, loggedIn)
+            return FollowPagerFragment.newInstance(defaultItem, loggedIn)
         }
-        if (firstLaunch && defaultItem != null) {
+        if (firstLaunch) {
             spinner.setSelection(
                 if (loggedIn) {
                     defaultItem
--- app/src/main/java/com/github/andreyasadchy/xtra/ui/follow/FollowPagerFragment.kt
+++ app/src/main/java/com/github/andreyasadchy/xtra/ui/follow/FollowPagerFragment.kt
@@ -5,13 +5,25 @@ import android.view.View
 import com.github.andreyasadchy.xtra.ui.common.pagers.MediaPagerFragment
 import com.github.andreyasadchy.xtra.ui.main.MainActivity
 
-class FollowPagerFragment(
-    private val defaultItem: Int?,
-    private val loggedIn: Boolean) : MediaPagerFragment() {
+class FollowPagerFragment : MediaPagerFragment() {
+
+    companion object {
+        private const val DEFAULT_ITEM = "default_item"
+        private const val LOGGED_IN = "logged_in"
+
+        fun newInstance(defaultItem: Int?, loggedIn: Boolean) = FollowPagerFragment().apply {
+            arguments = Bundle().apply {
+                putInt(DEFAULT_ITEM, defaultItem ?: 0)
+                putBoolean(LOGGED_IN, loggedIn)
+            }
+        }
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val activity = requireActivity() as MainActivity
+        val defaultItem = requireArguments().getInt(DEFAULT_ITEM)
+        val loggedIn = requireArguments().getBoolean(LOGGED_IN)
         setAdapter(adapter = FollowPagerAdapter(activity, childFragmentManager, loggedIn),
             currentItem = if (loggedIn) {
                 when (defaultItem) {

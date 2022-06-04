637fbd723becc24aa494b672006fff529e9ad5f9
Baptiste Candellier <baptiste@candellier.me>
2022-06-04 15:33:06 +0200

    feat: delete search fragment pager

--- app/src/main/java/com/github/andreyasadchy/xtra/ui/search/SearchFragment.kt
+++ app/src/main/java/com/github/andreyasadchy/xtra/ui/search/SearchFragment.kt
@@ -7,11 +7,13 @@ import android.view.ViewGroup
 import androidx.appcompat.widget.SearchView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import com.github.andreyasadchy.xtra.R
 import com.github.andreyasadchy.xtra.ui.Utils
 import com.github.andreyasadchy.xtra.ui.common.pagers.MediaPagerFragment
 import com.github.andreyasadchy.xtra.ui.main.MainActivity
+import com.github.andreyasadchy.xtra.ui.search.channels.ChannelSearchFragment
 import com.github.andreyasadchy.xtra.util.showKeyboard
 import kotlinx.android.synthetic.main.fragment_search.appBar
 import kotlinx.android.synthetic.main.fragment_search.search
@@ -29,22 +31,26 @@ class SearchFragment : MediaPagerFragment() {
         return inflater.inflate(R.layout.fragment_search, container, false)
     }
 
+    private var searchFragment: ChannelSearchFragment? = null
+    override val currentFragment: Fragment?
+        get() = searchFragment
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val activity = requireActivity() as MainActivity
-        val adapter = SearchPagerAdapter(activity, childFragmentManager).apply {
-            setOnItemChangedListener {
-                if (it.isResumed) {
-                    (it as Searchable).search(search.query.toString())
-                }
-            }
-        }
-        setAdapter(adapter)
+
         toolbar.apply {
             navigationIcon = Utils.getNavigationIcon(activity)
             setNavigationOnClickListener { activity.popFragment() }
         }
         search.showKeyboard()
+
+        searchFragment = ChannelSearchFragment().also { fragment ->
+            childFragmentManager
+                .beginTransaction()
+                .replace(R.id.fragmentContainer, fragment)
+                .commit()
+        }
 
         ViewCompat.setOnApplyWindowInsetsListener(appBar) { appBar, windowInsets ->
             val insets = windowInsets.getInsets(WindowInsetsCompat.Type.statusBars())
@@ -65,7 +71,7 @@ class SearchFragment : MediaPagerFragment() {
             private var job: Job? = null
 
             override fun onQueryTextSubmit(query: String): Boolean {
-                (currentFragment as? Searchable)?.search(query)
+                searchFragment?.search(query)
                 return false
             }
 
@@ -74,10 +80,10 @@ class SearchFragment : MediaPagerFragment() {
                 if (newText.isNotEmpty()) {
                     job = lifecycleScope.launchWhenResumed {
                         delay(750)
-                        (currentFragment as? Searchable)?.search(newText)
+                        searchFragment?.search(newText)
                     }
                 } else {
-                    (currentFragment as? Searchable)?.search(newText) // might be null on rotation, so as?
+                    searchFragment?.search(newText) // might be null on rotation, so as?
                 }
                 return false
             }

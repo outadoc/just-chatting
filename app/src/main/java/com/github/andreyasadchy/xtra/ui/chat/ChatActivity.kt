93e468a0126bc96f99210c04aad583cf7b37e6b0
Baptiste Candellier <baptiste@candellier.me>
2022-06-06 00:19:32 +0200

    Clean up code, fix pending intent issue

--- app/src/main/java/com/github/andreyasadchy/xtra/ui/chat/ChatActivity.kt
+++ app/src/main/java/com/github/andreyasadchy/xtra/ui/chat/ChatActivity.kt
@@ -35,6 +35,10 @@ class ChatActivity : BaseActivity() {
         ): Intent {
             return Intent(context, ChatActivity::class.java).apply {
                 action = Intent.ACTION_VIEW
+                flags = Intent.FLAG_ACTIVITY_NEW_DOCUMENT or
+                        Intent.FLAG_ACTIVITY_NEW_TASK or
+                        Intent.FLAG_ACTIVITY_CLEAR_TASK
+
                 putExtra(C.CHANNEL_ID, channelId)
                 putExtra(C.CHANNEL_LOGIN, channelLogin)
                 putExtra(C.CHANNEL_DISPLAYNAME, channelName)
@@ -58,69 +62,111 @@ class ChatActivity : BaseActivity() {
                 channelLogo = channelLogo
             )
 
-            val flags =
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) PendingIntent.FLAG_MUTABLE
-                else 0
+            val flags = PendingIntent.FLAG_UPDATE_CURRENT or
+                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) PendingIntent.FLAG_MUTABLE
+                    else 0
 
             val bubbleIntent = PendingIntent.getActivity(context, 0, target, flags)
 
             val request = ImageRequest.Builder(context)
                 .data(channelLogo)
+                .allowHardware(false)
                 .size(512, 512)
                 .target { drawable ->
                     val bitmap = drawable.toBitmap()
+                    val icon = IconCompat.createWithBitmap(bitmap)
 
                     val person: Person =
                         Person.Builder()
                             .setKey(channelId)
                             .setName(channelName)
+                            .setIcon(icon)
                             .build()
 
-                    val notificationId = channelId.hashCode()
-
-                    ShortcutManagerCompat.addDynamicShortcuts(
-                        context, listOf(
-                            ShortcutInfoCompat.Builder(context, channelId)
-                                .setIntent(target)
-                                .setLongLived(true)
-                                .setShortLabel(channelName)
-                                .build()
-                        )
-                    )
-
-                    NotificationManagerCompat.from(context).apply {
-                        createNotificationChannel(
-                            NotificationChannelCompat.Builder(
-                                CHANNEL_ID,
-                                NotificationManagerCompat.IMPORTANCE_DEFAULT
-                            )
-                                .setName("Chat bubbles")
-                                .build()
-                        )
-
-                        val icon = IconCompat.createWithBitmap(bitmap)
-
-                        notify(
-                            notificationId,
-                            NotificationCompat.Builder(context, CHANNEL_ID)
-                                .setContentIntent(bubbleIntent)
-                                .setSmallIcon(R.drawable.ic_send_black_24dp)
-                                .setBubbleMetadata(
-                                    NotificationCompat.BubbleMetadata.Builder(bubbleIntent, icon)
-                                        .setAutoExpandBubble(true)
-                                        .build()
-                                )
-                                .setLocusId(LocusIdCompat(channelId))
-                                .setStyle(NotificationCompat.MessagingStyle(person))
-                                .setShortcutId(channelId)
-                                .addPerson(person)
-                                .build()
-                        )
-                    }
+                    createShortcutForChannel(context, target, channelId, channelName, person)
+                    createGenericBubbleChannelIfNeeded(context)
+                    createBubbleChannelForUserIfNeeded(context, channelId, channelName)
+                    createBubble(context, channelId, bubbleIntent, icon, person)
                 }
                 .build()
 
             context.imageLoader.enqueue(request)
+        }
+
+        private fun notificationIdFor(channelId: String) = channelId.hashCode()
+
+        private fun createShortcutForChannel(
+            context: Context,
+            intent: Intent,
+            channelId: String,
+            channelName: String,
+            person: Person
+        ) {
+            ShortcutManagerCompat.addDynamicShortcuts(
+                context, listOf(
+                    ShortcutInfoCompat.Builder(context, channelId)
+                        .setIntent(intent)
+                        .setLongLived(true)
+                        .setShortLabel(channelName)
+                        .setPerson(person)
+                        .build()
+                )
+            )
+        }
+
+        private fun createGenericBubbleChannelIfNeeded(context: Context) =
+            NotificationManagerCompat.from(context).apply {
+                createNotificationChannel(
+                    NotificationChannelCompat.Builder(
+                        CHANNEL_ID,
+                        NotificationManagerCompat.IMPORTANCE_DEFAULT
+                    )
+                        .setName("Chat bubbles")
+                        .build()
+                )
+
+            }
+
+        private fun createBubbleChannelForUserIfNeeded(
+            context: Context,
+            channelId: String,
+            channelName: String
+        ) = NotificationManagerCompat.from(context).apply {
+            createNotificationChannel(
+                NotificationChannelCompat.Builder(
+                    channelId,
+                    NotificationManagerCompat.IMPORTANCE_DEFAULT
+                )
+                    .setName(channelName)
+                    .setConversationId(CHANNEL_ID, channelId)
+                    .build()
+            )
+        }
+
+        private fun createBubble(
+            context: Context,
+            channelId: String,
+            pendingIntent: PendingIntent,
+            icon: IconCompat,
+            person: Person
+        ) = NotificationManagerCompat.from(context).apply {
+            notify(
+                notificationIdFor(channelId),
+                NotificationCompat.Builder(context, channelId)
+                    .setContentIntent(pendingIntent)
+                    .setSmallIcon(R.drawable.ic_send_black_24dp)
+                    .setBubbleMetadata(
+                        NotificationCompat.BubbleMetadata.Builder(pendingIntent, icon)
+                            .setAutoExpandBubble(true)
+                            .setSuppressNotification(true)
+                            .build()
+                    )
+                    .setLocusId(LocusIdCompat(channelId))
+                    .setStyle(NotificationCompat.MessagingStyle(person))
+                    .setShortcutId(channelId)
+                    .addPerson(person)
+                    .build()
+            )
         }
     }
 

9477c6461f815c90bd4d6095e2c1ce1e2d8cbf57
Baptiste Candellier <baptiste@candellier.me>
2022-06-05 14:06:42 +0200

    fix: fix animations in Twitch emote picker

--- app/src/main/java/com/github/andreyasadchy/xtra/model/helix/emote/EmoteSetDeserializer.kt
+++ app/src/main/java/com/github/andreyasadchy/xtra/model/helix/emote/EmoteSetDeserializer.kt
@@ -6,9 +6,14 @@ import com.google.gson.JsonDeserializationContext
 import com.google.gson.JsonDeserializer
 import com.google.gson.JsonElement
 import com.google.gson.JsonParseException
+import okhttp3.HttpUrl.Companion.toHttpUrl
 import java.lang.reflect.Type
 
 class EmoteSetDeserializer : JsonDeserializer<EmoteSetResponse> {
+
+    companion object {
+        private const val BASE_EMOTE_URL = "https://static-cdn.jtvnw.net/emoticons/v2"
+    }
 
     @Throws(JsonParseException::class)
     override fun deserialize(
@@ -19,29 +24,52 @@ class EmoteSetDeserializer : JsonDeserializer<EmoteSetResponse> {
         val emotes = mutableListOf<TwitchEmote>()
         for (i in 0 until json.asJsonObject.getAsJsonArray("data").size()) {
             val emote = json.asJsonObject.getAsJsonArray("data").get(i).asJsonObject
-            val urls = emote.getAsJsonObject("images")
-            val url = urls.get(
-                when (ChatView.emoteQuality) {
-                    "4" -> ("url_4x")
-                    "3" -> ("url_4x")
-                    "2" -> ("url_2x")
-                    else -> ("url_1x")
-                }
-            ).takeUnless { it?.isJsonNull == true }?.asString ?: urls.get("url_2x")
-                .takeUnless { it?.isJsonNull == true }?.asString ?: urls.get("url_1x").asString
-            val format = if (emote.getAsJsonArray("format")
-                .first().asString.equals("animated")
-            ) "image/gif" else "image/png"
+
+            val preferredScale = when (ChatView.emoteQuality) {
+                "4" -> "4.0"
+                "3" -> "3.0"
+                "2" -> "2.0"
+                else -> "1.0"
+            }
+
+            val supportedFormats = emote.getAsJsonArray("format").map { it.asString }
+            val supportedScales = emote.getAsJsonArray("scale").map { it.asString }
+
+            val isAnimated = supportedFormats.any { it == "animated" }
+
             emotes.add(
                 TwitchEmote(
-                    emote.get("name").asString,
-                    type = format,
-                    url = url,
+                    name = emote.get("name").asString,
+                    type = if (isAnimated) "image/gif" else "image/png",
+                    url = createUrlForEmote(
+                        id = emote.get("id").asString,
+                        format = if (isAnimated) "animated" else "static",
+                        scale = supportedScales.firstOrNull { it == preferredScale }
+                            ?: supportedScales.last()
+                            ?: "1.0"
+                    ),
                     setId = emote.get("emote_set_id").asString,
                     ownerId = emote.get("owner_id").asString
                 )
             )
         }
+
         return EmoteSetResponse(emotes.sortedByDescending { it.setId })
+    }
+
+    private fun createUrlForEmote(
+        id: String,
+        format: String,
+        theme: String = "light",
+        scale: String
+    ): String {
+        return BASE_EMOTE_URL.toHttpUrl()
+            .newBuilder()
+            .addPathSegment(id)
+            .addPathSegment(format)
+            .addPathSegment(theme)
+            .addPathSegment(scale)
+            .build()
+            .toString()
     }
 }

57c32462bb43779b6d974c147560ce4283828774
Baptiste Candellier <baptiste@candellier.me>
2022-06-06 19:43:55 +0200

    chore: use proper menu on main fragment

--- app/src/main/java/com/github/andreyasadchy/xtra/ui/common/MediaFragment.kt
+++ app/src/main/java/com/github/andreyasadchy/xtra/ui/common/MediaFragment.kt
@@ -7,7 +7,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.app.AlertDialog
-import androidx.appcompat.widget.PopupMenu
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.fragment.app.Fragment
@@ -18,8 +17,7 @@ import com.github.andreyasadchy.xtra.ui.login.LoginActivity
 import com.github.andreyasadchy.xtra.ui.main.MainActivity
 import com.github.andreyasadchy.xtra.ui.settings.SettingsActivity
 import kotlinx.android.synthetic.main.fragment_media.appBar
-import kotlinx.android.synthetic.main.fragment_media.menu
-import kotlinx.android.synthetic.main.fragment_media.search
+import kotlinx.android.synthetic.main.fragment_media.toolbar
 
 abstract class MediaFragment : Fragment(), Scrollable {
 
@@ -46,61 +44,58 @@ abstract class MediaFragment : Fragment(), Scrollable {
 
         val user = User.get(requireContext())
 
-        search.setOnClickListener {
-            (activity as? MainActivity)?.openSearch()
-        }
+        toolbar.inflateMenu(R.menu.top_menu)
 
-        menu.setOnClickListener { it ->
-            PopupMenu(requireContext(), it).apply {
-                inflate(R.menu.top_menu)
+        toolbar.menu.findItem(R.id.login).title =
+            if (user !is NotLoggedIn) getString(R.string.log_out)
+            else getString(R.string.log_in)
 
-                menu.findItem(R.id.login).title =
-                    if (user !is NotLoggedIn) getString(R.string.log_out)
-                    else getString(R.string.log_in)
-
-                setOnMenuItemClickListener {
-                    when (it.itemId) {
-                        R.id.settings -> {
-                            activity?.startActivityFromFragment(
-                                this@MediaFragment,
-                                Intent(activity, SettingsActivity::class.java),
-                                3
-                            )
-                        }
-                        R.id.login -> {
-                            if (user is NotLoggedIn) {
-                                activity?.startActivityForResult(
-                                    Intent(
-                                        activity,
-                                        LoginActivity::class.java
-                                    ),
-                                    1
+        toolbar.setOnMenuItemClickListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.search -> {
+                    (activity as? MainActivity)?.openSearch()
+                    true
+                }
+                R.id.settings -> {
+                    activity?.startActivityFromFragment(
+                        this@MediaFragment,
+                        Intent(activity, SettingsActivity::class.java),
+                        3
+                    )
+                    true
+                }
+                R.id.login -> {
+                    if (user is NotLoggedIn) {
+                        activity?.startActivityForResult(
+                            Intent(
+                                activity,
+                                LoginActivity::class.java
+                            ),
+                            1
+                        )
+                    } else {
+                        AlertDialog.Builder(requireContext()).apply {
+                            setTitle(getString(R.string.logout_title))
+                            user.login?.let { user ->
+                                setMessage(
+                                    getString(
+                                        R.string.logout_msg,
+                                        user
+                                    )
                                 )
-                            } else {
-                                AlertDialog.Builder(requireContext()).apply {
-                                    setTitle(getString(R.string.logout_title))
-                                    user.login?.let { user ->
-                                        setMessage(
-                                            getString(
-                                                R.string.logout_msg,
-                                                user
-                                            )
-                                        )
-                                    }
-                                    setNegativeButton(getString(R.string.no)) { dialog, _ -> dialog.dismiss() }
-                                    setPositiveButton(getString(R.string.yes)) { _, _ ->
-                                        activity?.startActivityForResult(
-                                            Intent(activity, LoginActivity::class.java),
-                                            2
-                                        )
-                                    }
-                                }.show()
                             }
-                        }
+                            setNegativeButton(getString(R.string.no)) { dialog, _ -> dialog.dismiss() }
+                            setPositiveButton(getString(R.string.yes)) { _, _ ->
+                                activity?.startActivityForResult(
+                                    Intent(activity, LoginActivity::class.java),
+                                    2
+                                )
+                            }
+                        }.show()
                     }
                     true
                 }
-                show()
+                else -> false
             }
         }
 

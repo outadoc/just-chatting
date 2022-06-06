3141976204f1c11fdf5c59d552e172e6192d3aa7
Baptiste Candellier <baptiste@candellier.me>
2022-06-06 20:37:40 +0200

    refactor: merge media pager and follow pager fragment

--- app/src/main/java/com/github/andreyasadchy/xtra/ui/follow/FollowPagerFragment.kt
+++ app/src/main/java/com/github/andreyasadchy/xtra/ui/follow/FollowPagerFragment.kt
@@ -1,11 +1,19 @@
 package com.github.andreyasadchy.xtra.ui.follow
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
-import com.github.andreyasadchy.xtra.ui.common.pagers.MediaPagerFragment
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.github.andreyasadchy.xtra.R
+import com.github.andreyasadchy.xtra.ui.common.BaseNetworkFragment
+import com.github.andreyasadchy.xtra.ui.common.Scrollable
+import com.github.andreyasadchy.xtra.ui.common.pagers.ItemAwareFragmentPagerAdapter
+import com.github.andreyasadchy.xtra.ui.common.pagers.ItemAwarePagerFragment
 import com.github.andreyasadchy.xtra.ui.main.MainActivity
+import kotlinx.android.synthetic.main.fragment_media_pager.viewPager
 
-class FollowPagerFragment : MediaPagerFragment() {
+class FollowPagerFragment : BaseNetworkFragment(), ItemAwarePagerFragment, Scrollable {
 
     companion object {
         private const val LOGGED_IN = "logged_in"
@@ -16,6 +24,10 @@ class FollowPagerFragment : MediaPagerFragment() {
             }
         }
     }
+    private var adapter: ItemAwareFragmentPagerAdapter? = null
+
+    override val currentFragment: Fragment?
+        get() = adapter?.currentFragment
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -23,9 +35,30 @@ class FollowPagerFragment : MediaPagerFragment() {
         setAdapter(FollowPagerAdapter(activity, childFragmentManager))
     }
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_media_pager, container, false)
+    }
+
+    private fun setAdapter(adapter: ItemAwareFragmentPagerAdapter) {
+        this.adapter = adapter
+        viewPager.adapter = adapter
+        viewPager.currentItem = 0
+        viewPager.offscreenPageLimit = adapter.count
+    }
+
     override fun initialize() {
     }
 
     override fun onNetworkRestored() {
+    }
+
+    override fun scrollToTop() {
+        if (currentFragment is Scrollable) {
+            (currentFragment as Scrollable).scrollToTop()
+        }
     }
 }

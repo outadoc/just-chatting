a092c84818a24ae17e6d560876e6b39e75d7079c
Baptiste Candellier <baptiste@candellier.me>
2022-06-26 21:15:43 +0200

    feat: use compat API for accessibility check

--- app/src/main/java/com/github/andreyasadchy/xtra/ui/common/ColorExt.kt
+++ app/src/main/java/com/github/andreyasadchy/xtra/ui/common/ColorExt.kt
@@ -1,29 +1,10 @@
 package com.github.andreyasadchy.xtra.ui.common
 
-import android.graphics.Color
-import android.os.Build
 import androidx.annotation.ColorInt
-import androidx.annotation.RequiresApi
-import kotlin.math.max
-import kotlin.math.min
+import androidx.core.graphics.ColorUtils
 
-fun isContrastRatioAccessible(@ColorInt c1: Int, @ColorInt c2: Int): Boolean {
-    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-        val accessibleRatio = 4.5
-        val ratio = getContrastRatio(c1, c2)
-        ratio > accessibleRatio
-    } else {
-        true
-    }
-}
-
-@RequiresApi(Build.VERSION_CODES.O)
-private fun getContrastRatio(@ColorInt c1: Int, @ColorInt c2: Int): Double {
-    val l1 = Color.valueOf(c1).luminance()
-    val l2 = Color.valueOf(c2).luminance()
-
-    val brightest = max(l1, l2)
-    val darkest = min(l1, l2)
-
-    return (brightest + 0.05) / (darkest + 0.05)
+fun isContrastRatioAccessible(@ColorInt background: Int, @ColorInt foreground: Int): Boolean {
+    val accessibleRatio = 4.5
+    val ratio = ColorUtils.calculateContrast(background, foreground)
+    return ratio > accessibleRatio
 }

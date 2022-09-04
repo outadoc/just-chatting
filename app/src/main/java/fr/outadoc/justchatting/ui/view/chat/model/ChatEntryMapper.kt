6d14c2d3e40efe4a5987f856d338406c4f1721c9
Baptiste Candellier <baptiste@candellier.me>
2022-09-04 18:51:37 +0200

    feat: support primepaidupgrade msgid

--- app/src/main/java/fr/outadoc/justchatting/ui/view/chat/model/ChatEntryMapper.kt
+++ app/src/main/java/fr/outadoc/justchatting/ui/view/chat/model/ChatEntryMapper.kt
@@ -13,181 +13,179 @@ import fr.outadoc.justchatting.model.chat.UserState
 
 class ChatEntryMapper(private val context: Context) {
 
-    fun map(chatCommand: ChatCommand): ChatEntry? {
-        return with(chatCommand) {
-            when (this) {
-                is Command.ClearChat -> {
-                    ChatEntry.Highlighted(
-                        header = context.getString(R.string.chat_clear),
-                        data = null,
-                        timestamp = timestamp
-                    )
-                }
-                is Command.Ban -> {
-                    ChatEntry.Highlighted(
-                        header = context.getString(R.string.chat_ban, userLogin),
-                        data = null,
-                        timestamp = timestamp
-                    )
-                }
-                is Command.Timeout -> {
-                    ChatEntry.Highlighted(
-                        header = context.getString(R.string.chat_timeout, userLogin, duration),
-                        data = null,
-                        timestamp = timestamp
-                    )
-                }
-                is Command.ClearMessage -> {
-                    ChatEntry.Highlighted(
-                        header = context.getString(R.string.chat_clearmsg, userLogin, message),
-                        data = null,
-                        timestamp = timestamp
-                    )
-                }
-                is Command.Join -> {
-                    ChatEntry.Highlighted(
-                        header = context.getString(R.string.chat_join, channelLogin),
-                        data = null,
-                        timestamp = timestamp
-                    )
-                }
-                is Command.Disconnect -> {
-                    ChatEntry.Highlighted(
-                        header = context.getString(
-                            R.string.chat_disconnect,
-                            channelLogin,
-                            throwable
-                        ),
-                        data = null,
-                        timestamp = timestamp
-                    )
-                }
-                is Command.Notice -> {
-                    ChatEntry.Highlighted(
-                        header = messageId?.getNoticeString(context = context, message = message)
-                            ?: message,
-                        data = null,
-                        timestamp = timestamp
-                    )
-                }
-                is Command.SendMessageError -> {
-                    ChatEntry.Highlighted(
-                        header = context.getString(
-                            R.string.chat_send_msg_error,
-                            throwable.toString()
-                        ),
-                        data = null,
-                        timestamp = timestamp
-                    )
-                }
-                is Command.SocketError -> {
-                    ChatEntry.Highlighted(
-                        header = context.getString(
-                            R.string.chat_socket_error,
-                            throwable.toString()
-                        ),
-                        data = null,
-                        timestamp = timestamp
-                    )
-                }
-                is Command.UserNotice -> {
-                    if (userMessage == null) {
-                        ChatEntry.Highlighted(
-                            header = systemMsg,
-                            headerIconResId = msgId?.getMessageIdIcon(),
-                            data = null,
-                            timestamp = timestamp
-                        )
-                    } else {
-                        ChatEntry.Highlighted(
-                            header = systemMsg ?: msgId?.getMessageIdString(context),
-                            headerIconResId = msgId?.getMessageIdIcon(),
-                            data = ChatEntry.Data.Rich(
-                                message = userMessage.message,
-                                userId = userMessage.userId,
-                                userName = userMessage.userName,
-                                userLogin = userMessage.userLogin,
-                                isAction = userMessage.isAction,
-                                color = userMessage.color,
-                                emotes = userMessage.emotes,
-                                badges = userMessage.badges,
-                                inReplyTo = null
-                            ),
-                            timestamp = timestamp
-                        )
-                    }
-                }
-                is LiveChatMessage -> {
-                    val (header, icon) = when {
-                        systemMsg != null -> {
-                            systemMsg to msgId?.getMessageIdIcon()
-                        }
-                        msgId != null -> {
-                            msgId.getMessageIdString(context) to msgId.getMessageIdIcon()
-                        }
-                        isFirst -> {
-                            context.getString(R.string.chat_first) to R.drawable.ic_wave
-                        }
-                        rewardId != null -> {
-                            context.getString(R.string.chat_reward) to R.drawable.ic_toll
-                        }
-                        else -> {
-                            null to null
-                        }
-                    }
-
-                    if (header != null || isAction) {
-                        ChatEntry.Highlighted(
-                            header = header,
-                            headerIconResId = icon,
-                            data = ChatEntry.Data.Rich(
-                                message = message,
-                                userId = userId,
-                                userName = userName,
-                                userLogin = userLogin,
-                                isAction = isAction,
-                                color = color,
-                                emotes = emotes,
-                                badges = badges,
-                                inReplyTo = null
-                            ),
-                            timestamp = timestamp
-                        )
-                    } else {
-                        ChatEntry.Simple(
-                            ChatEntry.Data.Rich(
-                                message = message,
-                                userId = userId,
-                                userName = userName,
-                                userLogin = userLogin,
-                                isAction = false,
-                                color = color,
-                                emotes = emotes,
-                                badges = badges,
-                                inReplyTo = inReplyTo
-                            ),
-                            timestamp = timestamp
-                        )
-                    }
-                }
-                is PubSubPointReward -> {
-                    ChatEntry.Highlighted(
-                        header = context.getString(
-                            R.string.user_redeemed,
-                            userName,
-                            rewardTitle,
-                            rewardCost
-                        ),
-                        headerIconResId = R.drawable.ic_toll,
-                        data = null,
-                        timestamp = timestamp
-                    )
-                }
-                PingCommand,
-                is RoomState,
-                is UserState,
-                -> null
+    fun map(chatCommand: ChatCommand): ChatEntry? = with(chatCommand) {
+        when (this) {
+            is Command.ClearChat -> {
+                ChatEntry.Highlighted(
+                    header = context.getString(R.string.chat_clear),
+                    data = null,
+                    timestamp = timestamp
+                )
             }
+            is Command.Ban -> {
+                ChatEntry.Highlighted(
+                    header = context.getString(R.string.chat_ban, userLogin),
+                    data = null,
+                    timestamp = timestamp
+                )
+            }
+            is Command.Timeout -> {
+                ChatEntry.Highlighted(
+                    header = context.getString(R.string.chat_timeout, userLogin, duration),
+                    data = null,
+                    timestamp = timestamp
+                )
+            }
+            is Command.ClearMessage -> {
+                ChatEntry.Highlighted(
+                    header = context.getString(R.string.chat_clearmsg, userLogin, message),
+                    data = null,
+                    timestamp = timestamp
+                )
+            }
+            is Command.Join -> {
+                ChatEntry.Highlighted(
+                    header = context.getString(R.string.chat_join, channelLogin),
+                    data = null,
+                    timestamp = timestamp
+                )
+            }
+            is Command.Disconnect -> {
+                ChatEntry.Highlighted(
+                    header = context.getString(
+                        R.string.chat_disconnect,
+                        channelLogin,
+                        throwable
+                    ),
+                    data = null,
+                    timestamp = timestamp
+                )
+            }
+            is Command.Notice -> {
+                ChatEntry.Highlighted(
+                    header = messageId?.getNoticeString(context = context, message = message)
+                        ?: message,
+                    data = null,
+                    timestamp = timestamp
+                )
+            }
+            is Command.SendMessageError -> {
+                ChatEntry.Highlighted(
+                    header = context.getString(
+                        R.string.chat_send_msg_error,
+                        throwable.toString()
+                    ),
+                    data = null,
+                    timestamp = timestamp
+                )
+            }
+            is Command.SocketError -> {
+                ChatEntry.Highlighted(
+                    header = context.getString(
+                        R.string.chat_socket_error,
+                        throwable.toString()
+                    ),
+                    data = null,
+                    timestamp = timestamp
+                )
+            }
+            is Command.UserNotice -> {
+                if (userMessage == null) {
+                    ChatEntry.Highlighted(
+                        header = systemMsg,
+                        headerIconResId = msgId?.getMessageIdIcon(),
+                        data = null,
+                        timestamp = timestamp
+                    )
+                } else {
+                    ChatEntry.Highlighted(
+                        header = systemMsg ?: msgId?.getMessageIdString(context),
+                        headerIconResId = msgId?.getMessageIdIcon(),
+                        data = ChatEntry.Data.Rich(
+                            message = userMessage.message,
+                            userId = userMessage.userId,
+                            userName = userMessage.userName,
+                            userLogin = userMessage.userLogin,
+                            isAction = userMessage.isAction,
+                            color = userMessage.color,
+                            emotes = userMessage.emotes,
+                            badges = userMessage.badges,
+                            inReplyTo = null
+                        ),
+                        timestamp = timestamp
+                    )
+                }
+            }
+            is LiveChatMessage -> {
+                val (header, icon) = when {
+                    systemMsg != null -> {
+                        systemMsg to msgId?.getMessageIdIcon()
+                    }
+                    msgId != null -> {
+                        msgId.getMessageIdString(context) to msgId.getMessageIdIcon()
+                    }
+                    isFirst -> {
+                        context.getString(R.string.chat_first) to R.drawable.ic_wave
+                    }
+                    rewardId != null -> {
+                        context.getString(R.string.chat_reward) to R.drawable.ic_toll
+                    }
+                    else -> {
+                        null to null
+                    }
+                }
+
+                if (header != null || isAction) {
+                    ChatEntry.Highlighted(
+                        header = header,
+                        headerIconResId = icon,
+                        data = ChatEntry.Data.Rich(
+                            message = message,
+                            userId = userId,
+                            userName = userName,
+                            userLogin = userLogin,
+                            isAction = isAction,
+                            color = color,
+                            emotes = emotes,
+                            badges = badges,
+                            inReplyTo = null
+                        ),
+                        timestamp = timestamp
+                    )
+                } else {
+                    ChatEntry.Simple(
+                        ChatEntry.Data.Rich(
+                            message = message,
+                            userId = userId,
+                            userName = userName,
+                            userLogin = userLogin,
+                            isAction = false,
+                            color = color,
+                            emotes = emotes,
+                            badges = badges,
+                            inReplyTo = inReplyTo
+                        ),
+                        timestamp = timestamp
+                    )
+                }
+            }
+            is PubSubPointReward -> {
+                ChatEntry.Highlighted(
+                    header = context.getString(
+                        R.string.user_redeemed,
+                        userName,
+                        rewardTitle,
+                        rewardCost
+                    ),
+                    headerIconResId = R.drawable.ic_toll,
+                    data = null,
+                    timestamp = timestamp
+                )
+            }
+            PingCommand,
+            is RoomState,
+            is UserState,
+            -> null
         }
     }
 }
@@ -205,7 +203,7 @@ private fun String.getMessageIdIcon(): Int? {
     return when (this) {
         "highlighted-message" -> R.drawable.ic_sparkles
         "announcement" -> R.drawable.ic_campaign
-        "resub", "sub" -> R.drawable.ic_star
+        "resub", "sub", "primepaidupgrade" -> R.drawable.ic_star
         "submysterygift", "subgift" -> R.drawable.ic_redeem
         else -> null
     }

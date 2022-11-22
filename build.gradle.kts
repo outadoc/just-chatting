be7e1f45d9ee89d3371c376bf57e2b1af02baef2
Baptiste Candellier <baptiste@candellier.me>
2022-11-22 22:09:33 +0100

    feat(oss): generate dependency license report

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import com.github.jk1.license.importer.XmlReportImporter
+import com.github.jk1.license.render.JsonReportRenderer
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.android) apply false
@@ -5,4 +8,16 @@ plugins {
     alias(libs.plugins.kotlin.parcelize) apply false
     alias(libs.plugins.ksp) apply false
     alias(libs.plugins.spotless) apply false
+    alias(libs.plugins.licenseReport)
+}
+
+group = "fr.outadoc"
+
+licenseReport {
+    excludeOwnGroup = true
+    configurations = arrayOf("releaseRuntimeClasspath")
+    renderers = arrayOf(JsonReportRenderer())
+    importers = arrayOf(
+        XmlReportImporter("Other", file("dependencies.xml"))
+    )
 }

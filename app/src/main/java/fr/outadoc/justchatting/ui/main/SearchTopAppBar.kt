a1ec57ac5cd0fdd7bc5d99d1ebbc15277c683ad3
Baptiste Candellier <baptiste@candellier.me>
2022-11-28 22:36:59 +0100

    feat(search): make search bar rounded

--- app/src/main/java/fr/outadoc/justchatting/ui/main/SearchTopAppBar.kt
+++ app/src/main/java/fr/outadoc/justchatting/ui/main/SearchTopAppBar.kt
@@ -4,6 +4,7 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Cancel
@@ -11,17 +12,18 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.FocusRequester
 import androidx.compose.ui.focus.focusRequester
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import fr.outadoc.justchatting.R
 import fr.outadoc.justchatting.ui.HapticIconButton
-import fr.outadoc.justchatting.ui.chat.ExpandedTopAppBar
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -31,38 +33,40 @@ fun SearchTopAppBar(
     onQueryChange: (String) -> Unit
 ) {
     val focusRequester = FocusRequester()
-    ExpandedTopAppBar(
-        modifier = modifier.statusBarsPadding(),
-        title = { Text(stringResource(R.string.search)) }
-    ) {
-        TextField(
-            modifier = Modifier
-                .padding(horizontal = 16.dp)
-                .padding(bottom = 8.dp)
-                .focusRequester(focusRequester)
-                .fillMaxWidth(),
-            placeholder = { Text(stringResource(R.string.search_hint)) },
-            value = query,
-            onValueChange = onQueryChange,
-            singleLine = true,
-            keyboardOptions = KeyboardOptions(
-                imeAction = ImeAction.Search,
-                autoCorrect = false
-            ),
-            trailingIcon = {
-                AnimatedVisibility(visible = query.isNotEmpty()) {
-                    HapticIconButton(onClick = { onQueryChange("") }) {
-                        Icon(
-                            Icons.Filled.Cancel,
-                            contentDescription = "Clear query"
-                        )
-                    }
+    TextField(
+        modifier = modifier
+            .statusBarsPadding()
+            .padding(16.dp)
+            .focusRequester(focusRequester)
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(percent = 50),
+        colors = TextFieldDefaults.textFieldColors(
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+            disabledIndicatorColor = Color.Transparent,
+            errorIndicatorColor = Color.Transparent
+        ),
+        placeholder = { Text(stringResource(R.string.search_hint)) },
+        value = query,
+        onValueChange = onQueryChange,
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(
+            imeAction = ImeAction.Search,
+            autoCorrect = false
+        ),
+        trailingIcon = {
+            AnimatedVisibility(visible = query.isNotEmpty()) {
+                HapticIconButton(onClick = { onQueryChange("") }) {
+                    Icon(
+                        Icons.Filled.Cancel,
+                        contentDescription = "Clear query"
+                    )
                 }
             }
-        )
-
-        LaunchedEffect(onQueryChange) {
-            focusRequester.requestFocus()
         }
+    )
+
+    LaunchedEffect(onQueryChange) {
+        focusRequester.requestFocus()
     }
 }

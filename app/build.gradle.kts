28646dabdee4a31af6e77c5ed4724e71ff057967
Baptiste Candellier <baptiste@candellier.me>
2022-11-28 23:11:35 +0100

    chore(versioning): set version to 0.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "fr.outadoc.justchatting"
         minSdk = 21
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "0.2.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 

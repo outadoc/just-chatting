4ca0899905a4f81e416506899a6aabf58af1fd8b
Baptiste Candellier <baptiste@candellier.me>
2023-02-04 21:58:50 +0100

    feat(settings): implement new emote kill-switches

--- app/src/main/java/fr/outadoc/justchatting/di/TwitchModule.kt
+++ app/src/main/java/fr/outadoc/justchatting/di/TwitchModule.kt
@@ -36,7 +36,7 @@ val twitchModule = module {
         )
     }
 
-    single { EmotesRepository(get(), get(), get(), get()) }
+    single { EmotesRepository(get(), get(), get(), get(), get()) }
 
     single<TwitchRepository> { TwitchRepositoryImpl(get(), get()) }
 
--- component-chatapi-domain/src/main/java/fr/outadoc/justchatting/component/chatapi/domain/repository/EmotesRepository.kt
+++ component-chatapi-domain/src/main/java/fr/outadoc/justchatting/component/chatapi/domain/repository/EmotesRepository.kt
@@ -6,12 +6,14 @@ import fr.outadoc.justchatting.component.chatapi.db.MaxRecentEmotes
 import fr.outadoc.justchatting.component.chatapi.db.RecentEmotesDao
 import fr.outadoc.justchatting.component.chatapi.domain.model.RecentEmote
 import fr.outadoc.justchatting.component.chatapi.domain.model.TwitchBadge
+import fr.outadoc.justchatting.component.preferences.domain.PreferenceRepository
 import fr.outadoc.justchatting.component.twitch.http.api.BttvEmotesApi
 import fr.outadoc.justchatting.component.twitch.http.api.StvEmotesApi
 import fr.outadoc.justchatting.component.twitch.http.api.TwitchBadgesApi
 import fr.outadoc.justchatting.component.twitch.utils.map
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.withContext
 
@@ -20,6 +22,7 @@ class EmotesRepository(
     private val stvEmotesApi: StvEmotesApi,
     private val bttvEmotesApi: BttvEmotesApi,
     private val recentEmotes: RecentEmotesDao,
+    private val preferencesRepository: PreferenceRepository,
 ) {
     suspend fun loadGlobalBadges(): List<TwitchBadge> =
         withContext(Dispatchers.IO) {
@@ -65,36 +68,60 @@ class EmotesRepository(
 
     suspend fun loadGlobalStvEmotes(): List<Emote> =
         withContext(Dispatchers.IO) {
+            if (!preferencesRepository.currentPreferences.first().enableStvEmotes) {
+                return@withContext emptyList()
+            }
+
             stvEmotesApi.getGlobalStvEmotes()
                 .map { emote -> emote.map() }
         }
 
     suspend fun loadStvEmotes(channelId: String): List<Emote> =
         withContext(Dispatchers.IO) {
+            if (!preferencesRepository.currentPreferences.first().enableStvEmotes) {
+                return@withContext emptyList()
+            }
+
             stvEmotesApi.getStvEmotes(channelId)
                 .map { emote -> emote.map() }
         }
 
     suspend fun loadGlobalBttvEmotes(): List<Emote> =
         withContext(Dispatchers.IO) {
+            if (!preferencesRepository.currentPreferences.first().enableBttvEmotes) {
+                return@withContext emptyList()
+            }
+
             bttvEmotesApi.getGlobalBttvEmotes()
                 .map { emote -> emote.map() }
         }
 
     suspend fun loadBttvEmotes(channelId: String): List<Emote> =
         withContext(Dispatchers.IO) {
+            if (!preferencesRepository.currentPreferences.first().enableBttvEmotes) {
+                return@withContext emptyList()
+            }
+
             bttvEmotesApi.getBttvEmotes(channelId).allEmotes
                 .map { emote -> emote.map() }
         }
 
     suspend fun loadBttvGlobalFfzEmotes(): List<Emote> =
         withContext(Dispatchers.IO) {
+            if (!preferencesRepository.currentPreferences.first().enableFfzEmotes) {
+                return@withContext emptyList()
+            }
+
             bttvEmotesApi.getBttvGlobalFfzEmotes()
                 .map { emote -> emote.map() }
         }
 
     suspend fun loadBttvFfzEmotes(channelId: String): List<Emote> =
         withContext(Dispatchers.IO) {
+            if (!preferencesRepository.currentPreferences.first().enableFfzEmotes) {
+                return@withContext emptyList()
+            }
+
             bttvEmotesApi.getBttvFfzEmotes(channelId)
                 .map { emote -> emote.map() }
         }

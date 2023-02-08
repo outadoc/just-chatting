291178818172672458e285e798e6e0c8fbf38c3d
Baptiste Candellier <baptiste@candellier.me>
2023-02-09 00:19:19 +0100

    fix(roomstate): fix default room state visibility

--- feature-chat-presentation/src/main/java/fr/outadoc/justchatting/feature/chat/presentation/RoomState.kt
+++ feature-chat-presentation/src/main/java/fr/outadoc/justchatting/feature/chat/presentation/RoomState.kt
@@ -2,11 +2,12 @@ package fr.outadoc.justchatting.feature.chat.presentation
 
 import androidx.compose.runtime.Immutable
 import kotlin.time.Duration
+import kotlin.time.Duration.Companion.minutes
 
 @Immutable
 data class RoomState(
     val isEmoteOnly: Boolean = false,
-    val minFollowDuration: Duration = -Duration.INFINITE,
+    val minFollowDuration: Duration = (-1).minutes,
     val uniqueMessagesOnly: Boolean = false,
     val slowModeDuration: Duration = Duration.ZERO,
     val isSubOnly: Boolean = false,

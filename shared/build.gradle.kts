e37cba643a9d048acd6e8ed4718ebab9315bb3e2
Baptiste Candellier <baptiste@candellier.me>
2023-10-10 19:58:42 +0200

    Move deps to common deps

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -27,6 +27,7 @@ kotlin {
                 implementation(libs.kotlinx.datetime)
                 implementation(libs.kotlinx.serialization.json)
                 implementation(libs.ktor.client.core)
+                implementation(libs.moko.resources.core)
             }
         }
 
@@ -52,13 +53,7 @@ kotlin {
                 implementation(libs.compose.ui.core)
                 implementation(libs.compose.ui.tooling)
                 implementation(libs.koin.compose)
-                implementation(libs.kotlinx.collections.immutable)
-                implementation(libs.kotlinx.coroutines)
-                implementation(libs.kotlinx.datetime)
-                implementation(libs.kotlinx.serialization.json)
-                implementation(libs.ktor.client.core)
                 implementation(libs.material.core)
-                implementation(libs.moko.resources.core)
                 implementation(libs.moko.resources.compose)
             }
 

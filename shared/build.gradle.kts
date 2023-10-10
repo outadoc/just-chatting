7b5a8818f75a0f8288ee7513865ed272f99a3816
Baptiste Candellier <baptiste@candellier.me>
2023-10-11 00:44:22 +0200

    chore: make iOS targets explicit

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -17,7 +17,16 @@ kotlin {
         }
     }
 
-    ios()
+    listOf(
+        iosX64(),
+        iosArm64(),
+        iosSimulatorArm64()
+    ).forEach { iosTarget ->
+        iosTarget.binaries.framework {
+            baseName = "Shared"
+            isStatic = true
+        }
+    }
 
     sourceSets {
         val commonMain by getting {
@@ -60,15 +69,20 @@ kotlin {
             }
         }
 
+        val iosX64Main by getting
+        val iosArm64Main by getting
+        val iosSimulatorArm64Main by getting
+        val iosMain by creating {
+            dependsOn(commonMain)
+            iosX64Main.dependsOn(this)
+            iosArm64Main.dependsOn(this)
+            iosSimulatorArm64Main.dependsOn(this)
+        }
+
         val androidUnitTest by getting {
             dependencies {
                 implementation(libs.junit)
             }
-        }
-
-        val iosMain by getting {
-            dependsOn(commonMain)
-            dependencies {}
         }
     }
 }

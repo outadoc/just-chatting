409b9182bceeeaa62744385dbed1a85a2b7bbe33
Baptiste Candellier <baptiste@candellier.me>
2023-10-11 19:29:41 +0200

    Create iOS ViewModel

--- shared/src/commonMain/kotlin/fr/outadoc/justchatting/lifecycle/ViewModel.kt
+++ shared/src/commonMain/kotlin/fr/outadoc/justchatting/lifecycle/ViewModel.kt
@@ -2,6 +2,9 @@ package fr.outadoc.justchatting.lifecycle
 
 import kotlinx.coroutines.CoroutineScope
 
+/**
+ * https://proandroiddev.com/multiplatform-presenters-or-viewmodels-the-lean-way-cbb763c803af
+ */
 expect abstract class ViewModel() {
     val viewModelScope: CoroutineScope
     protected open fun onCleared()
---
+++ shared/src/iosMain/kotlin/fr/outadoc/justchatting/lifecycle/ViewModel.kt
@@ -0,0 +1,16 @@
+package fr.outadoc.justchatting.lifecycle
+
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.cancel
+
+actual abstract class ViewModel {
+
+    actual val viewModelScope = MainScope()
+
+    protected actual open fun onCleared() {}
+
+    fun clear() {
+        onCleared()
+        viewModelScope.cancel()
+    }
+}

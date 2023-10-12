6bb8a80d6afa75816a8235d256c06207c4107a72
Baptiste Candellier <baptiste@candellier.me>
2023-10-12 08:35:33 +0200

    fix(di): fix missing interface qualifier in injection

--- app/src/androidMain/kotlin/fr/outadoc/justchatting/di/SettingsModule.kt
+++ app/src/androidMain/kotlin/fr/outadoc/justchatting/di/SettingsModule.kt
@@ -4,6 +4,7 @@ import fr.outadoc.justchatting.component.preferences.domain.PreferenceRepository
 import fr.outadoc.justchatting.component.preferences.domain.SharedPrefsPreferenceRepository
 import fr.outadoc.justchatting.feature.preferences.presentation.AndroidLogRepository
 import fr.outadoc.justchatting.feature.preferences.presentation.DefaultReadExternalDependenciesList
+import fr.outadoc.justchatting.feature.preferences.presentation.LogRepository
 import fr.outadoc.justchatting.feature.preferences.presentation.ReadExternalDependenciesList
 import fr.outadoc.justchatting.feature.preferences.presentation.SettingsViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
@@ -11,7 +12,7 @@ import org.koin.dsl.module
 
 val settingsModule = module {
     viewModel { SettingsViewModel(get(), get(), get()) }
-    single { AndroidLogRepository(get()) }
+    single<LogRepository> { AndroidLogRepository(get()) }
     single<ReadExternalDependenciesList> { DefaultReadExternalDependenciesList(get()) }
     single<PreferenceRepository> { SharedPrefsPreferenceRepository(get()) }
 }

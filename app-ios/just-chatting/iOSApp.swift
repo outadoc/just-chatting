5676263af54fc1596c3e2fbb216610bc4b472ad7
Baptiste Candellier <baptiste@candellier.me>
2023-10-17 18:55:51 +0200

    Enable logger

--- app-ios/just-chatting/iOSApp.swift
+++ app-ios/just-chatting/iOSApp.swift
@@ -7,6 +7,10 @@ struct iOSApp: App {
     init() {
         // Perform dependency injection
         Container.shared.setup()
+
+        #if DEBUG
+        Logger.shared.logStrategy = AppleLogStrategy()
+        #endif
     }
 
     var body: some Scene {

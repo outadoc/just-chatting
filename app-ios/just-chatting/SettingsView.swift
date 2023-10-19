847fe279faf8ef3f91b4a8b6ddc91912cd742fe8
Baptiste Candellier <baptiste@candellier.me>
2023-10-19 19:17:54 +0200

    Basic working settings update

--- app-ios/just-chatting/SettingsView.swift
+++ app-ios/just-chatting/SettingsView.swift
@@ -11,17 +11,84 @@ import SwiftUI
 import Swinject
 
 struct SettingsView: View {
-    @State var isOn = true
+    @StateObject private var viewModel = ViewModel(wrapped: Container.shared.resolve(SettingsViewModel.self)!)
+    var body: some View {
+        VStack {
+            if let appPreferences = viewModel.appPreferences {
+                InnerSettingsView(
+                    appPreferences: appPreferences,
+                    updatePreferences: { newPrefs in
+                        viewModel.update(appPreferences: newPrefs)
+                    }
+                )
+            }
+        }
+        .task {
+            await viewModel.activate()
+        }
+    }
+}
+
+private struct InnerSettingsView: View {
+    var appPreferences: AppPreferences
+    var updatePreferences: (AppPreferences) -> Void
+
+    private var enableRecentMessages: Binding<Bool> {
+        Binding {
+            appPreferences.enableRecentMessages
+        } set: { value in
+            updatePreferences(
+                AppPreferences(
+                    appUser: appPreferences.appUser,
+                    showTimestamps: appPreferences.showTimestamps,
+                    enableRecentMessages: value,
+                    enableFfzEmotes: appPreferences.enableFfzEmotes,
+                    enableStvEmotes: appPreferences.enableStvEmotes,
+                    enableBttvEmotes: appPreferences.enableBttvEmotes,
+                    enablePronouns: appPreferences.enablePronouns,
+                    enableNotifications: appPreferences.enableNotifications
+                )
+            )
+        }
+    }
+
     var body: some View {
         NavigationView {
             Form {
                 Section(header: Text(MR.strings.shared.settings_thirdparty_header.desc().localized())) {
-                    Toggle(isOn: $isOn) {
+                    Toggle(isOn: enableRecentMessages) {
                         Text(MR.strings.shared.settings_thirdparty_recent_title.desc().localized())
                     }
                 }
             }
             .navigationTitle(Text(MR.strings.shared.settings.desc().localized()))
+        }
+    }
+}
+
+private extension SettingsView {
+    @MainActor
+    private class ViewModel: ObservableObject {
+        let wrapped: SettingsViewModel
+        init(wrapped: SettingsViewModel) {
+            self.wrapped = wrapped
+        }
+
+        @Published
+        private(set) var appPreferences: AppPreferences? = nil
+
+        func update(appPreferences: AppPreferences) {
+            wrapped.updatePreferences(appPreferences: appPreferences)
+        }
+
+        func activate() async {
+            await withTaskGroup(of: Void.self) { taskGroup in
+                taskGroup.addTask { @MainActor in
+                    for await appPreferences in self.wrapped.appPreferences {
+                        self.appPreferences = appPreferences
+                    }
+                }
+            }
         }
     }
 }

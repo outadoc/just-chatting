066b9aa8d0c0f4604a36e9adf2a1d44ff9e34d55
Baptiste Candellier <baptiste@candellier.me>
2023-10-23 19:25:43 +0200

    feat(ios): use os_log for logging on darwin

--- shared/src/iosMain/kotlin/fr/outadoc/justchatting/utils/logging/AppleLogStrategy.kt
+++ shared/src/iosMain/kotlin/fr/outadoc/justchatting/utils/logging/AppleLogStrategy.kt
@@ -1,10 +1,31 @@
 package fr.outadoc.justchatting.utils.logging
 
-import platform.Foundation.NSLog
+import kotlinx.cinterop.ExperimentalForeignApi
+import kotlinx.cinterop.ptr
+import platform.darwin.OS_LOG_DEFAULT
+import platform.darwin.OS_LOG_TYPE_DEBUG
+import platform.darwin.OS_LOG_TYPE_DEFAULT
+import platform.darwin.OS_LOG_TYPE_ERROR
+import platform.darwin.OS_LOG_TYPE_FAULT
+import platform.darwin.OS_LOG_TYPE_INFO
+import platform.darwin.__dso_handle
+import platform.darwin._os_log_internal
 
 object AppleLogStrategy : LogStrategy {
 
+    @OptIn(ExperimentalForeignApi::class)
     override fun println(level: Logger.Level, tag: String?, content: String) {
-        NSLog("[${level.tag}][$tag]\t$content".replace("%", "%%"))
+        _os_log_internal(
+            dso = __dso_handle.ptr,
+            log = OS_LOG_DEFAULT,
+            type = when (level) {
+                Logger.Level.Verbose -> OS_LOG_TYPE_DEBUG
+                Logger.Level.Debug -> OS_LOG_TYPE_INFO
+                Logger.Level.Info -> OS_LOG_TYPE_DEFAULT
+                Logger.Level.Warning -> OS_LOG_TYPE_ERROR
+                Logger.Level.Error -> OS_LOG_TYPE_FAULT
+            },
+            message = "[$tag] $content".replace("%", "%%"),
+        )
     }
 }

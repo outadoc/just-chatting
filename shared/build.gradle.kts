cae10ef03c203db768398e1f6ded93fa56e33231
Baptiste Candellier <baptiste@candellier.me>
2024-05-24 19:05:20 +0200

    Try to fix gradle files

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -19,7 +19,7 @@ kotlin {
         }
     }
 
-    jvm("desktop") {
+    jvm {
         compilations.all {
             kotlinOptions {
                 jvmTarget = "17"
@@ -47,99 +47,76 @@ kotlin {
     }
 
     sourceSets {
-        val commonMain by getting {
-            dependencies {
-                api(libs.kotlinx.coroutines)
-                api(libs.kotlinx.datetime)
-                api(libs.kotlinx.serialization.json)
-                api(libs.ktor.client.core)
-                api(libs.moko.resources.core)
+        commonMain.dependencies {
+            api(libs.kotlinx.coroutines)
+            api(libs.kotlinx.datetime)
+            api(libs.kotlinx.serialization.json)
+            api(libs.ktor.client.core)
+            api(libs.moko.resources.core)
 
-                implementation(compose.runtime)
+            implementation(compose.runtime)
 
-                implementation(libs.androidx.paging.common)
-                implementation(libs.androidx.paging.compose.common)
-                implementation(libs.bignum)
-                implementation(libs.fluid.currency)
-                implementation(libs.koin.core)
-                implementation(libs.kotlinx.collections.immutable)
-                implementation(libs.kotlinx.datetime)
-                implementation(libs.kotlinx.serialization.json)
-                implementation(libs.ktor.client.auth)
-                implementation(libs.ktor.client.websockets)
-                implementation(libs.ktor.contentNegociation)
-                implementation(libs.ktor.logging)
-                implementation(libs.ktor.serialization)
-                implementation(libs.sqldelight.coroutines)
-                implementation(libs.uri.kmp)
-                implementation(libs.unicode.codepoints)
-            }
+            implementation(libs.androidx.paging.common)
+            implementation(libs.androidx.paging.compose.common)
+            implementation(libs.bignum)
+            implementation(libs.fluid.currency)
+            implementation(libs.koin.core)
+            implementation(libs.kotlinx.collections.immutable)
+            implementation(libs.kotlinx.datetime)
+            implementation(libs.kotlinx.serialization.json)
+            implementation(libs.ktor.client.auth)
+            implementation(libs.ktor.client.websockets)
+            implementation(libs.ktor.contentNegociation)
+            implementation(libs.ktor.logging)
+            implementation(libs.ktor.serialization)
+            implementation(libs.sqldelight.coroutines)
+            implementation(libs.uri.kmp)
+            implementation(libs.unicode.codepoints)
         }
 
-        val jvmMain by creating {
-            dependsOn(commonMain)
-            dependencies {
-                implementation(compose.material)
-                implementation(compose.material3)
-                implementation(compose.materialIconsExtended)
-                implementation(compose.ui)
-                implementation(compose.uiTooling)
-                implementation(libs.compose.material.windowSizeClass)
-            }
+        jvmMain.dependencies {
+            implementation(compose.material)
+            implementation(compose.material3)
+            implementation(compose.materialIconsExtended)
+            implementation(compose.ui)
+            implementation(compose.uiTooling)
+            implementation(libs.compose.material.windowSizeClass)
         }
 
-        val androidMain by getting {
-            dependsOn(jvmMain)
-            dependencies {
-                implementation(libs.accompanist.permissions)
-                implementation(libs.accompanist.placeholder)
-                implementation(libs.accompanist.systemuicontroller)
-                implementation(libs.androidx.activity.compose)
-                implementation(libs.androidx.appcompat)
-                implementation(libs.androidx.browser)
-                implementation(libs.androidx.datastore.preferences)
-                implementation(libs.androidx.emoji2.core)
-                implementation(libs.androidx.palette)
-                implementation(libs.androidx.paging.runtime.android)
-                implementation(libs.androidx.splashscreen)
-                implementation(libs.coil.compose)
-                implementation(libs.koin.compose)
-                implementation(libs.ktor.client.cio)
-                implementation(libs.material.core)
-                implementation(libs.moko.resources.compose)
+        androidMain.dependencies {
+            implementation(libs.accompanist.permissions)
+            implementation(libs.accompanist.placeholder)
+            implementation(libs.accompanist.systemuicontroller)
+            implementation(libs.androidx.activity.compose)
+            implementation(libs.androidx.appcompat)
+            implementation(libs.androidx.browser)
+            implementation(libs.androidx.datastore.preferences)
+            implementation(libs.androidx.emoji2.core)
+            implementation(libs.androidx.palette)
+            implementation(libs.androidx.paging.runtime.android)
+            implementation(libs.androidx.splashscreen)
+            implementation(libs.coil.compose)
+            implementation(libs.koin.compose)
+            implementation(libs.ktor.client.cio)
+            implementation(libs.material.core)
+            implementation(libs.moko.resources.compose)
 
-                api(libs.sqldelight.driver.android)
-            }
+            api(libs.sqldelight.driver.android)
         }
 
-        val desktopMain by getting {
-            dependsOn(jvmMain)
-            dependencies {
-                implementation(compose.desktop.common)
-                implementation(compose.desktop.currentOs)
-            }
+        jvmMain.dependencies {
+            implementation(compose.desktop.common)
+            implementation(compose.desktop.currentOs)
         }
 
-        val iosX64Main by getting
-        val iosArm64Main by getting
-        val iosSimulatorArm64Main by getting
-        val iosMain by creating {
-            dependsOn(commonMain)
-            iosX64Main.dependsOn(this)
-            iosArm64Main.dependsOn(this)
-            iosSimulatorArm64Main.dependsOn(this)
-
-            dependencies {
-                api(libs.sqldelight.driver.native)
-                implementation(libs.androidx.paging.runtime.ios)
-                implementation(libs.ktor.client.darwin)
-            }
+        iosMain.dependencies {
+            api(libs.sqldelight.driver.native)
+            implementation(libs.androidx.paging.runtime.ios)
+            implementation(libs.ktor.client.darwin)
         }
 
-        val androidUnitTest by getting {
-            dependencies {
-                implementation(libs.junit)
-            }
+        androidNativeTest.dependencies {
+            implementation(libs.junit)
         }
     }
 }

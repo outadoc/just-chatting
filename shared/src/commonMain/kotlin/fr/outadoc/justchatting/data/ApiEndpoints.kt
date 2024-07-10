067c829998052d7a1ea0d892b9d7bb09ead664ad
Baptiste Candellier <baptiste@candellier.me>
2024-07-10 22:38:50 +0200

    fix(api): fix base url for auth API

--- shared/src/commonMain/kotlin/fr/outadoc/justchatting/data/ApiEndpoints.kt
+++ shared/src/commonMain/kotlin/fr/outadoc/justchatting/data/ApiEndpoints.kt
@@ -1,13 +1,13 @@
 package fr.outadoc.justchatting.data
 
 internal object ApiEndpoints {
-    const val ALEJO_PRONOUNS = "https://pronouns.alejo.io/api/"
-    const val BTTV_BASE = "https://api.betterttv.net/"
+    const val ALEJO_PRONOUNS = "https://pronouns.alejo.io/api"
+    const val BTTV_BASE = "https://api.betterttv.net"
     const val BTTV_EMOTE_CDN = "https://cdn.betterttv.net/emote"
-    const val RECENT_MESSAGES = "https://recent-messages.robotty.de/api/"
-    const val STV_BASE = "https://7tv.io/"
-    const val STV_EMOTE_CDN = "https://cdn.7tv.app/emote/"
-    const val TWITCH_AUTH = "https://id.twitch.tv/oauth2/"
+    const val RECENT_MESSAGES = "https://recent-messages.robotty.de/api"
+    const val STV_BASE = "https://7tv.io"
+    const val STV_EMOTE_CDN = "https://cdn.7tv.app/emote"
+    const val TWITCH_AUTH = "https://id.twitch.tv/oauth2"
     const val TWITCH_EMOTE_CDN = "https://static-cdn.jtvnw.net/emoticons"
-    const val TWITCH_HELIX = "https://api.twitch.tv/helix/"
+    const val TWITCH_HELIX = "https://api.twitch.tv/helix"
 }

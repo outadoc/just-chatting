997d4e4c69c078d09ce047b904441b934da814e1
Baptiste Candellier <baptiste@candellier.me>
2024-07-17 17:22:03 +0200

    Implement VM (?)

--- shared/src/commonMain/kotlin/fr/outadoc/justchatting/feature/home/presentation/EpgViewModel.kt
+++ shared/src/commonMain/kotlin/fr/outadoc/justchatting/feature/home/presentation/EpgViewModel.kt
@@ -1,8 +1,34 @@
 package fr.outadoc.justchatting.feature.home.presentation
 
+import androidx.paging.PagingData
+import fr.outadoc.justchatting.feature.home.domain.GetScheduleForFollowedChannelsUseCase
 import fr.outadoc.justchatting.feature.home.domain.TwitchRepository
+import fr.outadoc.justchatting.feature.home.domain.model.ChannelSchedule
 import fr.outadoc.justchatting.utils.presentation.ViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.launch
 
 internal class EpgViewModel(
-    private val twitchRepository: TwitchRepository,
-) : ViewModel()
+    private val getScheduleForFollowedChannels: GetScheduleForFollowedChannelsUseCase
+) : ViewModel() {
+
+    sealed class State {
+        data object Loading : State()
+        data class Loaded(
+            val pagingData: Flow<PagingData<ChannelSchedule>>
+        ) : State()
+    }
+
+    private val _state = MutableStateFlow<State>(State.Loading)
+    val state: Flow<State> = _state
+
+    fun load() {
+        viewModelScope.launch {
+            _state.value = State.Loading
+            _state.value = State.Loaded(
+                pagingData = getScheduleForFollowedChannels()
+            )
+        }
+    }
+}

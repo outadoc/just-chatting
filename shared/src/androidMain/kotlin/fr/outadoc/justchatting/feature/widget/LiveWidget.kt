8c2607983bb491fee603f41da74fbf7a910841bd
Baptiste Candellier <baptiste@candellier.me>
2024-08-24 12:45:02 +0200

    Proper scaffold

--- shared/src/androidMain/kotlin/fr/outadoc/justchatting/feature/widget/LiveWidget.kt
+++ shared/src/androidMain/kotlin/fr/outadoc/justchatting/feature/widget/LiveWidget.kt
@@ -1,40 +1,35 @@
 package fr.outadoc.justchatting.feature.widget
 
 import android.content.Context
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.res.stringResource
 import androidx.glance.Button
 import androidx.glance.GlanceId
-import androidx.glance.GlanceModifier
 import androidx.glance.GlanceTheme
+import androidx.glance.ImageProvider
+import androidx.glance.LocalContext
 import androidx.glance.action.actionStartActivity
 import androidx.glance.appwidget.GlanceAppWidget
+import androidx.glance.appwidget.components.Scaffold
+import androidx.glance.appwidget.components.TitleBar
 import androidx.glance.appwidget.provideContent
-import androidx.glance.background
 import androidx.glance.layout.Alignment
-import androidx.glance.layout.Column
 import androidx.glance.layout.Row
-import androidx.glance.layout.fillMaxSize
-import androidx.glance.layout.padding
-import androidx.glance.text.Text
 import fr.outadoc.justchatting.feature.shared.presentation.mobile.MainActivity
+import fr.outadoc.justchatting.shared.R
 
 internal class LiveWidget : GlanceAppWidget() {
 
     override suspend fun provideGlance(context: Context, id: GlanceId) {
         provideContent {
             GlanceTheme(colors = GlanceTheme.colors) {
-                Column(
-                    modifier = GlanceModifier
-                        .fillMaxSize()
-                        .background(GlanceTheme.colors.background),
-                    verticalAlignment = Alignment.Top,
-                    horizontalAlignment = Alignment.CenterHorizontally,
+                Scaffold(
+                    titleBar = {
+                        TitleBar(
+                            startIcon = ImageProvider(R.drawable.ic_notif),
+                            title = LocalContext.current.getString(R.string.app_name),
+                        )
+                    }
                 ) {
-                    Text(
-                        text = "Where to?",
-                        modifier = GlanceModifier.padding(12.dp),
-                    )
-
                     Row(
                         horizontalAlignment = Alignment.CenterHorizontally,
                     ) {

b1926b86e259a4e39dd1c7088352346244f16a9c
Baptiste Candellier <baptiste@candellier.me>
2024-09-13 13:41:11 +0200

    chore(kmp): rename files with platform suffixes

--- shared/src/androidMain/kotlin/fr/outadoc/justchatting/utils/presentation/ContextExt.kt
+++ shared/src/androidMain/kotlin/fr/outadoc/justchatting/utils/presentation/ContextExt.kt
@@ -4,10 +4,6 @@ import android.content.Context
 import android.widget.Toast
 import androidx.annotation.StringRes
 
-internal fun Context.toast(@StringRes resId: Int) {
-    Toast.makeText(this, resId, Toast.LENGTH_LONG).show()
-}
-
 internal fun Context.shortToast(@StringRes resId: Int) {
     Toast.makeText(this, resId, Toast.LENGTH_SHORT).show()
 }

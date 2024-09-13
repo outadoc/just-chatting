9debd20547bf7760f183b7de185d26a9a05035a6
Baptiste Candellier <baptiste@candellier.me>
2024-09-13 14:24:29 +0200

    chore(notifications): create noop chat notifier

---
+++ shared/src/commonMain/kotlin/fr/outadoc/justchatting/feature/chat/presentation/NoopChatNotifier.kt
@@ -0,0 +1,15 @@
+package fr.outadoc.justchatting.feature.chat.presentation
+
+import coil3.PlatformContext
+import fr.outadoc.justchatting.feature.shared.domain.model.User
+
+internal class NoopChatNotifier : ChatNotifier {
+
+    override val areNotificationsEnabled: Boolean = false
+
+    override fun notify(context: PlatformContext, user: User) {
+    }
+
+    override fun dismissNotification(context: PlatformContext, channelId: String) {
+    }
+}

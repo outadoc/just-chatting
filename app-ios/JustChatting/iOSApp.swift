6a4132459500ed76b81300a26f30e9728e456006
Baptiste Candellier <baptiste@candellier.me>
2024-09-15 19:47:59 +0200

    feat(logging/ios): re-enable iOS logger

--- app-ios/JustChatting/iOSApp.swift
+++ app-ios/JustChatting/iOSApp.swift
@@ -7,6 +7,10 @@ struct iOSApp: App {
     init() {
         // Perform dependency injection
         SharedKoinKt.startSharedKoin { _ in }
+
+        #if DEBUG
+            Logger.shared.logStrategy = AppleLogStrategy()
+        #endif
     }
 
     var body: some Scene {
--- shared/src/iosMain/kotlin/fr/outadoc/justchatting/utils/logging/AppleLogStrategy.kt
+++ shared/src/iosMain/kotlin/fr/outadoc/justchatting/utils/logging/AppleLogStrategy.kt
@@ -11,7 +11,7 @@ import platform.darwin.OS_LOG_TYPE_INFO
 import platform.darwin.__dso_handle
 import platform.darwin._os_log_internal
 
-internal object AppleLogStrategy : LogStrategy {
+public object AppleLogStrategy : LogStrategy {
 
     @OptIn(ExperimentalForeignApi::class)
     override fun println(level: Logger.Level, tag: String?, content: String) {

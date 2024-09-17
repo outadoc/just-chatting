a8bce0adb857b6fb6654006e7cfb300822db7473
Baptiste Candellier <baptiste@candellier.me>
2024-09-17 15:01:39 +0200

    feat(logging/ios): better logging metadata for iOS

--- shared/src/iosMain/kotlin/fr/outadoc/justchatting/utils/logging/AppleLogStrategy.kt
+++ shared/src/iosMain/kotlin/fr/outadoc/justchatting/utils/logging/AppleLogStrategy.kt
@@ -2,7 +2,6 @@ package fr.outadoc.justchatting.utils.logging
 
 import kotlinx.cinterop.ExperimentalForeignApi
 import kotlinx.cinterop.ptr
-import platform.darwin.OS_LOG_DEFAULT
 import platform.darwin.OS_LOG_TYPE_DEBUG
 import platform.darwin.OS_LOG_TYPE_DEFAULT
 import platform.darwin.OS_LOG_TYPE_ERROR
@@ -10,14 +9,22 @@ import platform.darwin.OS_LOG_TYPE_FAULT
 import platform.darwin.OS_LOG_TYPE_INFO
 import platform.darwin.__dso_handle
 import platform.darwin._os_log_internal
+import platform.darwin.os_log_create
+import platform.darwin.os_log_t
 
 public object AppleLogStrategy : LogStrategy {
 
     @OptIn(ExperimentalForeignApi::class)
     override fun println(level: Logger.Level, tag: String?, content: String) {
+        val logger: os_log_t =
+            os_log_create(
+                subsystem = "fr.outadoc.justchatting",
+                category = tag ?: "JustChatting",
+            )
+
         _os_log_internal(
             dso = __dso_handle.ptr,
-            log = OS_LOG_DEFAULT,
+            log = logger,
             type = when (level) {
                 Logger.Level.Verbose -> OS_LOG_TYPE_DEBUG
                 Logger.Level.Debug -> OS_LOG_TYPE_INFO
@@ -25,7 +32,7 @@ public object AppleLogStrategy : LogStrategy {
                 Logger.Level.Warning -> OS_LOG_TYPE_ERROR
                 Logger.Level.Error -> OS_LOG_TYPE_FAULT
             },
-            message = "[$tag] $content".replace("%", "%%"),
+            message = content.replace("%", "%%"),
         )
     }
 }

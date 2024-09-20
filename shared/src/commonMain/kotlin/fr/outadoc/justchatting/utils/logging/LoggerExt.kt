8f1a4811ea00caa65d6247fa928b8799150c24d6
Baptiste Candellier <baptiste@candellier.me>
2024-09-20 23:21:05 +0200

    chore(color): use common parseHexColor impl for android as well

--- shared/src/commonMain/kotlin/fr/outadoc/justchatting/utils/logging/LoggerExt.kt
+++ shared/src/commonMain/kotlin/fr/outadoc/justchatting/utils/logging/LoggerExt.kt
@@ -1,24 +1,25 @@
 package fr.outadoc.justchatting.utils.logging
 
-internal inline fun <reified T : Any> logVerbose(noinline content: () -> String) =
-    Logger.println(Logger.Level.Verbose, T::class.simpleName!!, content)
+internal inline fun logVerbose(tag: String, noinline content: () -> String) =
+    Logger.println(Logger.Level.Verbose, tag, content)
 
-internal inline fun <reified T : Any> logDebug(noinline content: () -> String) =
-    Logger.println(Logger.Level.Debug, T::class.simpleName!!, content)
+internal inline fun logDebug(tag: String, noinline content: () -> String) =
+    Logger.println(Logger.Level.Debug, tag, content)
 
-internal inline fun <reified T : Any> logInfo(noinline content: () -> String) =
-    Logger.println(Logger.Level.Info, T::class.simpleName!!, content)
+internal inline fun logInfo(tag: String, noinline content: () -> String) =
+    Logger.println(Logger.Level.Info, tag, content)
 
-internal inline fun <reified T : Any> logWarning(noinline content: () -> String) =
-    Logger.println(Logger.Level.Warning, T::class.simpleName!!, content)
+internal inline fun logWarning(tag: String, noinline content: () -> String) =
+    Logger.println(Logger.Level.Warning, tag, content)
 
-internal inline fun <reified T : Any> logError(
+internal inline fun logError(
+    tag: String,
     throwable: Throwable? = null,
     noinline content: () -> String,
 ) {
     Logger.println(
         level = Logger.Level.Error,
-        tag = T::class.simpleName!!,
+        tag = tag,
         content = if (throwable != null) {
             {
                 buildString {
@@ -30,4 +31,23 @@ internal inline fun <reified T : Any> logError(
             content
         },
     )
+}
+
+internal inline fun <reified T : Any> logVerbose(noinline content: () -> String) =
+    logVerbose(T::class.simpleName!!, content)
+
+internal inline fun <reified T : Any> logDebug(noinline content: () -> String) =
+    logDebug(T::class.simpleName!!, content)
+
+internal inline fun <reified T : Any> logInfo(noinline content: () -> String) =
+    logInfo(T::class.simpleName!!, content)
+
+internal inline fun <reified T : Any> logWarning(noinline content: () -> String) =
+    logWarning(T::class.simpleName!!, content)
+
+internal inline fun <reified T : Any> logError(
+    throwable: Throwable? = null,
+    noinline content: () -> String,
+) {
+    return logError(T::class.simpleName!!, throwable, content)
 }
--- shared/src/commonMain/kotlin/fr/outadoc/justchatting/utils/presentation/ColorExt.kt
+++ shared/src/commonMain/kotlin/fr/outadoc/justchatting/utils/presentation/ColorExt.kt
@@ -2,6 +2,16 @@ package fr.outadoc.justchatting.utils.presentation
 
 import androidx.compose.runtime.Stable
 import androidx.compose.ui.graphics.Color
+import fr.outadoc.justchatting.utils.logging.logError
 
+@OptIn(ExperimentalStdlibApi::class)
 @Stable
-internal expect fun String.parseHexColor(): Color?
+internal fun String.parseHexColor(): Color? {
+    return try {
+        val color: Long = removePrefix("#").lowercase().hexToLong()
+        Color(color).copy(alpha = 1f)
+    } catch (e: Exception) {
+        logError("ColorExt", e) { "Failed to parse color from string: $this" }
+        null
+    }
+}

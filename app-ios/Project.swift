246a5761cbffd24480bc868ec8d0851c59a4793c
Baptiste Candellier <baptiste@candellier.me>
2024-09-27 20:19:48 +0200

    chore(tuist/ios): use tuist to configure xcode project in code (#444)

---
+++ app-ios/Project.swift
@@ -0,0 +1,51 @@
+import ProjectDescription
+
+import ProjectDescription
+
+let project = Project(
+    name: "JustChatting",
+    settings: .settings(base: [
+        "ENABLE_USER_SCRIPT_SANDBOXING": "NO",
+        "ASSETCATALOG_COMPILER_GENERATE_SWIFT_ASSET_SYMBOL_EXTENSIONS": "NO",
+        "FRAMEWORK_SEARCH_PATHS": "$(SRCROOT)/../shared/build/xcode-frameworks/$(CONFIGURATION)/$(SDK_NAME)",
+        "SWIFT_VERSION": "6.0"
+    ]),
+    targets: [
+        .target(
+            name: "JustChatting",
+            destinations: .iOS,
+            product: .app,
+            bundleId: "fr.outadoc.justchatting",
+            deploymentTargets: .iOS("17.0.0"),
+            infoPlist: .extendingDefault(
+                with: [
+                    "CFBundleShortVersionString": "0.1.0",
+                    "CFBundleVersion": "0.1.0",
+                    "CFBundleLocalizations": ["en", "fr"],
+                    "CFBundleURLTypes": [
+                        Plist.Value.dictionary(
+                            [
+                                "CFBundleTypeRole": "Editor",
+                                "CFBundleURLName": "fr.outadoc.justchatting",
+                                "CFBundleURLSchemes": ["justchatting"],
+                            ]
+                        ),
+                    ],
+                    "UILaunchStoryboardName": "Launch Screen.storyboard",
+                    "CADisableMinimumFrameDurationOnPhone": true,
+                    "LSApplicationCategoryType": "public.app-category.social-networking",
+                ]
+            ),
+            sources: ["JustChatting/Sources/**"],
+            resources: .resources(["JustChatting/Resources/**"]),
+            scripts: [
+                .pre(
+                    script: "cd \"$SRCROOT/..\" && ./gradlew :shared:embedAndSignAppleFrameworkForXcode",
+                    name: "Generate shared framework",
+                    basedOnDependencyAnalysis: false
+                ),
+            ],
+            dependencies: []
+        ),
+    ]
+)

5dae380dd10c5c6795a83a5c8fbb6b6091d2de49
Baptiste Candellier <baptiste@candellier.me>
2024-09-30 22:01:19 +0200

    chore(compose): add missing `@Stable` annotations

--- shared/src/androidMain/kotlin/fr/outadoc/justchatting/utils/presentation/StringFormatExt.android.kt
+++ shared/src/androidMain/kotlin/fr/outadoc/justchatting/utils/presentation/StringFormatExt.android.kt
@@ -1,15 +1,18 @@
 package fr.outadoc.justchatting.utils.presentation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
 import androidx.compose.ui.platform.LocalContext
 import dev.icerock.moko.resources.desc.PluralFormattedStringDesc
 import dev.icerock.moko.resources.desc.ResourceFormattedStringDesc
 
+@Stable
 @Composable
 internal actual fun PluralFormattedStringDesc.asString(): String {
     return toString(LocalContext.current)
 }
 
+@Stable
 @Composable
 internal actual fun ResourceFormattedStringDesc.asString(): String {
     return toString(LocalContext.current)
--- shared/src/iosMain/kotlin/fr/outadoc/justchatting/utils/presentation/StringFormatExt.ios.kt
+++ shared/src/iosMain/kotlin/fr/outadoc/justchatting/utils/presentation/StringFormatExt.ios.kt
@@ -1,14 +1,17 @@
 package fr.outadoc.justchatting.utils.presentation
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Stable
 import dev.icerock.moko.resources.desc.PluralFormattedStringDesc
 import dev.icerock.moko.resources.desc.ResourceFormattedStringDesc
 
+@Stable
 @Composable
 internal actual fun PluralFormattedStringDesc.asString(): String {
     return localized()
 }
 
+@Stable
 @Composable
 internal actual fun ResourceFormattedStringDesc.asString(): String {
     return localized()

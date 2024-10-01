36a9b52d2cb262d43ca9c886f30da643d50af0b9
Baptiste Candellier <baptiste@candellier.me>
2024-10-01 21:44:28 +0200

    chore(url/ios): revert custom UriHandler, fixed upstream

--- shared/src/iosMain/kotlin/fr/outadoc/justchatting/feature/shared/presentation/MainViewController.kt
+++ shared/src/iosMain/kotlin/fr/outadoc/justchatting/feature/shared/presentation/MainViewController.kt
@@ -1,6 +1,5 @@
 package fr.outadoc.justchatting.feature.shared.presentation
 
-import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.platform.LocalUriHandler
 import androidx.compose.ui.window.ComposeUIViewController
 import com.eygraber.uri.toNSURL
@@ -10,18 +9,16 @@ import platform.UIKit.UIViewController
 
 public fun getMainViewController(onShowAuthPage: (NSURL) -> Unit): UIViewController =
     ComposeUIViewController {
-        CompositionLocalProvider(LocalUriHandler provides AppleUriHandler) {
-            val uriHandler = LocalUriHandler.current
-            App(
-                onOpenAccessibilityPreferences = {
-                    uriHandler.openUri("app-settings:")
-                },
-                onOpenNotificationPreferences = {},
-                onOpenBubblePreferences = {},
-                onShareLogs = {},
-                onShowAuthPage = { uri ->
-                    uri.toNSURL()?.let(onShowAuthPage)
-                },
-            )
-        }
+        val uriHandler = LocalUriHandler.current
+        App(
+            onOpenAccessibilityPreferences = {
+                uriHandler.openUri("app-settings:")
+            },
+            onOpenNotificationPreferences = {},
+            onOpenBubblePreferences = {},
+            onShareLogs = {},
+            onShowAuthPage = { uri ->
+                uri.toNSURL()?.let(onShowAuthPage)
+            },
+        )
     }

0f29e58cde9d558546b0430e565b6dc4b64f6261
Baptiste Candellier <baptiste@candellier.me>
2025-04-21 16:49:43 +0200

    feat(redirect): try calling local webserver

--- auth/assets/redirect.js
+++ auth/assets/redirect.js
@@ -1,14 +1,27 @@
-function getRedirectUri() {
+function redirect() {
   let redirect = new URL("justchatting://auth/callback");
 
   redirect.hash = window.location.hash;
   redirect.search = window.location.search;
 
-  return redirect;
+  window.location = redirect;
 }
 
-function redirect() {
-  window.location = getRedirectUri();
+function callLocalServer() {
+  const localServer = new XMLHttpRequest();
+  const uri = "http://localhost:3000/auth/callback" + window.location.search + window.location.hash;
+
+  localServer.open("GET", uri, true);
+  localServer.onreadystatechange = function () {
+    if (localServer.readyState === 4 && localServer.status === 200) {
+      console.log("Local server called successfully.");
+    } else if (localServer.readyState === 4) {
+      console.error("Failed to call local server.");
+    }
+  };
+
+  localServer.send();
 }
 
+callLocalServer();
 redirect();

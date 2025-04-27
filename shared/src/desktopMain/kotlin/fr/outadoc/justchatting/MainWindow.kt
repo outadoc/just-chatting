dff87e5107f048dd4202ed7551aea110ce0c117d
Baptiste Candellier <baptiste@candellier.me>
2025-04-27 20:59:01 +0200

    feat(desktop): add icon to window

--- shared/src/desktopMain/kotlin/fr/outadoc/justchatting/MainWindow.kt
+++ shared/src/desktopMain/kotlin/fr/outadoc/justchatting/MainWindow.kt
@@ -5,8 +5,13 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import fr.outadoc.justchatting.di.startSharedKoin
 import fr.outadoc.justchatting.feature.shared.presentation.mobile.App
+import fr.outadoc.justchatting.shared.Res
+import fr.outadoc.justchatting.shared.app_name
+import fr.outadoc.justchatting.shared.icon_masked
 import fr.outadoc.justchatting.utils.logging.JvmLogStrategy
 import fr.outadoc.justchatting.utils.logging.Logger
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.resources.stringResource
 
 public fun startApp() {
     Logger.logStrategy = JvmLogStrategy
@@ -14,8 +19,9 @@ public fun startApp() {
 
     application {
         Window(
-            title = "Just Chatting",
+            title = stringResource(Res.string.app_name),
             onCloseRequest = ::exitApplication,
+            icon = painterResource(Res.drawable.icon_masked),
         ) {
             val uriHandler = LocalUriHandler.current
             App(

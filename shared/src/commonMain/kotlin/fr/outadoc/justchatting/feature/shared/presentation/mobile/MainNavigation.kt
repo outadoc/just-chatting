6be6e35355b027543c71cb2402e355c64ea3790f
Baptiste Candellier <baptiste@candellier.me>
2025-07-19 15:09:46 +0200

    chore(nav): use standard navigation rail

--- shared/src/commonMain/kotlin/fr/outadoc/justchatting/feature/shared/presentation/mobile/MainNavigation.kt
+++ shared/src/commonMain/kotlin/fr/outadoc/justchatting/feature/shared/presentation/mobile/MainNavigation.kt
@@ -1,5 +1,6 @@
 package fr.outadoc.justchatting.feature.shared.presentation.mobile
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.WindowInsets
@@ -17,6 +18,8 @@ import androidx.compose.material3.ScaffoldDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
 import androidx.compose.material3.adaptive.navigationsuite.NavigationSuite
+import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteDefaults
+import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteItem
 import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffoldDefaults
 import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteScaffoldLayout
 import androidx.compose.material3.adaptive.navigationsuite.NavigationSuiteType
@@ -42,121 +45,63 @@ internal fun MainNavigation(
 
     NavigationSuiteScaffoldLayout(
         navigationSuite = {
-            when (navSuiteType) {
-                NavigationSuiteType.NavigationRail -> {
-                    NavigationRail {
-                        Spacer(Modifier.weight(1f))
-
-                        NavigationRailItem(
-                            selected = selectedScreen == Screen.Timeline,
-                            onClick = { onSelectedTabChange(Screen.Timeline) },
-                            icon = {
-                                Icon(
-                                    imageVector = Icons.Filled.Home,
-                                    contentDescription = null,
-                                )
-                            },
-                            label = { Text(stringResource(Res.string.timeline_title)) },
-                        )
-
-                        NavigationRailItem(
-                            selected = selectedScreen == Screen.Followed,
-                            onClick = { onSelectedTabChange(Screen.Followed) },
-                            icon = {
-                                Icon(
-                                    imageVector = Icons.Filled.Favorite,
-                                    contentDescription = null,
-                                )
-                            },
-                            label = { Text(stringResource(Res.string.channels)) },
-                        )
-
-                        NavigationRailItem(
-                            selected = selectedScreen == Screen.Search,
-                            onClick = { onSelectedTabChange(Screen.Search) },
-                            icon = {
-                                Icon(
-                                    imageVector = Icons.Filled.Search,
-                                    contentDescription = null,
-                                )
-                            },
-                            label = { Text(stringResource(Res.string.search)) },
-                        )
-
-                        NavigationRailItem(
-                            selected = selectedScreen == Screen.Settings,
-                            onClick = { onSelectedTabChange(Screen.Settings) },
-                            icon = {
-                                Icon(
-                                    imageVector = Icons.Default.AccountCircle,
-                                    contentDescription = null,
-                                )
-                            },
-                            label = { Text(stringResource(Res.string.settings)) },
-                        )
-
-                        Spacer(Modifier.weight(1f))
-                    }
-                }
-
-                else -> {
-                    NavigationSuite(
-                        modifier = modifier,
-                        layoutType = navSuiteType,
-                        content = {
-                            item(
-                                selected = selectedScreen == Screen.Timeline,
-                                onClick = { onSelectedTabChange(Screen.Timeline) },
-                                icon = {
-                                    Icon(
-                                        imageVector = Icons.Filled.Home,
-                                        contentDescription = null,
-                                    )
-                                },
-                                label = { Text(stringResource(Res.string.timeline_title)) },
-                            )
-
-                            item(
-                                selected = selectedScreen == Screen.Followed,
-                                onClick = { onSelectedTabChange(Screen.Followed) },
-                                icon = {
-                                    Icon(
-                                        imageVector = Icons.Filled.Favorite,
-                                        contentDescription = null,
-                                    )
-                                },
-                                label = { Text(stringResource(Res.string.channels)) },
-                            )
-
-                            item(
-                                selected = selectedScreen == Screen.Search,
-                                onClick = { onSelectedTabChange(Screen.Search) },
-                                icon = {
-                                    Icon(
-                                        imageVector = Icons.Filled.Search,
-                                        contentDescription = null,
-                                    )
-                                },
-                                label = { Text(stringResource(Res.string.search)) },
-                            )
-
-                            item(
-                                selected = selectedScreen == Screen.Settings,
-                                onClick = { onSelectedTabChange(Screen.Settings) },
-                                icon = {
-                                    Icon(
-                                        imageVector = Icons.Default.AccountCircle,
-                                        contentDescription = null,
-                                    )
-                                },
-                                label = { Text(stringResource(Res.string.settings)) },
+            NavigationSuite(
+                modifier = modifier,
+                navigationSuiteType = navSuiteType,
+                verticalArrangement = Arrangement.Center,
+                content = {
+                    NavigationSuiteItem(
+                        selected = selectedScreen == Screen.Timeline,
+                        onClick = { onSelectedTabChange(Screen.Timeline) },
+                        icon = {
+                            Icon(
+                                imageVector = Icons.Filled.Home,
+                                contentDescription = null,
                             )
                         },
+                        label = { Text(stringResource(Res.string.timeline_title)) },
                     )
-                }
-            }
+
+                    NavigationSuiteItem(
+                        selected = selectedScreen == Screen.Followed,
+                        onClick = { onSelectedTabChange(Screen.Followed) },
+                        icon = {
+                            Icon(
+                                imageVector = Icons.Filled.Favorite,
+                                contentDescription = null,
+                            )
+                        },
+                        label = { Text(stringResource(Res.string.channels)) },
+                    )
+
+                    NavigationSuiteItem(
+                        selected = selectedScreen == Screen.Search,
+                        onClick = { onSelectedTabChange(Screen.Search) },
+                        icon = {
+                            Icon(
+                                imageVector = Icons.Filled.Search,
+                                contentDescription = null,
+                            )
+                        },
+                        label = { Text(stringResource(Res.string.search)) },
+                    )
+
+                    NavigationSuiteItem(
+                        selected = selectedScreen == Screen.Settings,
+                        onClick = { onSelectedTabChange(Screen.Settings) },
+                        icon = {
+                            Icon(
+                                imageVector = Icons.Default.AccountCircle,
+                                contentDescription = null,
+                            )
+                        },
+                        label = { Text(stringResource(Res.string.settings)) },
+                    )
+                },
+            )
         },
         content = {
+            // TODO check if this is still needed
             Scaffold(
                 topBar = topBar,
                 content = content,

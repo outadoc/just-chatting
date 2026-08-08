9587f27738daad7c8ba5df632de623a1a4f53918
Baptiste Candellier <baptiste@candellier.me>
2026-08-08 19:58:34 +0200

    fix: fix unexpected socket reconnections

--- shared/src/commonMain/kotlin/fr/outadoc/justchatting/utils/core/ConnectivityNetworkStateObserver.kt
+++ shared/src/commonMain/kotlin/fr/outadoc/justchatting/utils/core/ConnectivityNetworkStateObserver.kt
@@ -2,6 +2,7 @@ package fr.outadoc.justchatting.utils.core
 
 import dev.jordond.connectivity.Connectivity
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.map
 
 internal class ConnectivityNetworkStateObserver(
@@ -9,15 +10,16 @@ internal class ConnectivityNetworkStateObserver(
 ) : NetworkStateObserver {
     override val state: Flow<NetworkStateObserver.NetworkState>
         get() =
-            connectivity.statusUpdates.map { status ->
-                when (status) {
-                    is Connectivity.Status.Connected -> {
-                        NetworkStateObserver.NetworkState.Available
-                    }
+            connectivity.statusUpdates
+                .map { status ->
+                    when (status) {
+                        is Connectivity.Status.Connected -> {
+                            NetworkStateObserver.NetworkState.Available
+                        }
 
-                    Connectivity.Status.Disconnected -> {
-                        NetworkStateObserver.NetworkState.Unavailable
+                        Connectivity.Status.Disconnected -> {
+                            NetworkStateObserver.NetworkState.Unavailable
+                        }
                     }
-                }
-            }
+                }.distinctUntilChanged()
 }
